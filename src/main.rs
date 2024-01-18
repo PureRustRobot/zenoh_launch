c3e0c1adb8fbc9e8719925e67c7d7a3225c292e0
motii <108280115+motii8128@users.noreply.github.com>
2024-01-18 10:48:51 +0900

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,35 @@ use async_std;
 use zenoh::Error;
 
 use keyboard_publisher::wasd_controller;
-use command_controller::wheel_controller;
+use motor_controller::cmd_vel_to_wheel;
+use serial_transporter::serial_transporter;
 
 #[async_std::main]
 async fn main()->Result<(), Error>
 {
-    let key_task = async_std::task::spawn(wasd_controller("keyboard_publisher", "./param/config.yaml"));
-    let get_command_task = async_std::task::spawn(wheel_controller("wheel_cmd_publisher", "./param/config.yaml"));
+    let key_task = async_std::task::spawn(wasd_controller(
+        "keyboard_publisher", 
+        "cmd_vel", 
+        1.0)
+    );
+
+    let wheel_task = async_std::task::spawn(cmd_vel_to_wheel(
+        "cmd_to_wheel", 
+        "cmd_vel", 
+        "cmd/wheel", 
+        1.0)
+    );
+
+    let serial_task = async_std::task::spawn(serial_transporter(
+        "wheel_serial", 
+        "cmd/wheel", 
+        "/dev/ttyACM0", 
+        115200)
+    );
 
     key_task.await?;
-    get_command_task.await?;
+    wheel_task.await?;
+    serial_task.await?;  
 
     Ok(())
 }
