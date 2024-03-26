f2dba74056911933ec1f0ee56d7e3bd1b177bda8
motii <108280115+motii8128@users.noreply.github.com>
2024-03-26 10:23:21 +0900

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,14 @@ async fn main()->Result<(), Error>
 
     let cmd_convert_task = async_std::task::spawn(gamecon_to_cmd_vel("gamecon_to_cmd_vel", "game_con", "cmd_vel/wheel", false));
 
-    let cmd_to_wheel = async_std::task::spawn(cmd_vel_to_wheel("cmd_vel_to_wheel", "cmd_vel/wheel", "motor/wheel", 1.0));
+    let cmd_to_wheel = async_std::task::spawn(cmd_vel_to_wheel("cmd_vel_to_wheel", "cmd_vel/wheel", "motor/wheel", 1.0, false));
 
     let wheel_to_esp32 = async_std::task::spawn(wheel_bridge(
         "to_esp32", 
         "motor/wheel", 
         "192.168.4.2:8080", 
         "192.168.4.1:10000", 
-        false));
+        true));
 
     controller_task.await?;
     cmd_convert_task.await?;
