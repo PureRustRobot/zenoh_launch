068fdc87a4b2896cf392da68db3909627e4b806e
motii <108280115+motii8128@users.noreply.github.com>
2024-03-26 10:21:02 +0900

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,31 @@
 use async_std;
 use zenoh::Error;
 
-use keyboard_publisher::wasd_controller;
+use joy_publisher::joy_publisher;
+use command_controller::gamecon_to_cmd_vel;
 use motor_controller::cmd_vel_to_wheel;
-use serial_transporter::serial_transporter;
+use udp_bridge::wheel_bridge;
 
 #[async_std::main]
 async fn main()->Result<(), Error>
 {
-    let key_task = async_std::task::spawn(wasd_controller(
-        "keyboard_publisher", 
-        "cmd_vel", 
-        1.0)
-    );
+    let controller_task = async_std::task::spawn(joy_publisher("joy_publisher", "game_con", "ble"));
 
-    let wheel_task = async_std::task::spawn(cmd_vel_to_wheel(
-        "cmd_to_wheel", 
-        "cmd_vel", 
-        "cmd/wheel", 
-        1.0)
-    );
+    let cmd_convert_task = async_std::task::spawn(gamecon_to_cmd_vel("gamecon_to_cmd_vel", "game_con", "cmd_vel/wheel", false));
 
-    let serial_task = async_std::task::spawn(serial_transporter(
-        "wheel_serial", 
-        "cmd/wheel", 
-        "/dev/ttyACM0", 
-        115200)
-    );
+    let cmd_to_wheel = async_std::task::spawn(cmd_vel_to_wheel("cmd_vel_to_wheel", "cmd_vel/wheel", "motor/wheel", 1.0));
 
-    key_task.await?;
-    wheel_task.await?;
-    serial_task.await?;  
+    let wheel_to_esp32 = async_std::task::spawn(wheel_bridge(
+        "to_esp32", 
+        "motor/wheel", 
+        "192.168.4.2:8080", 
+        "192.168.4.1:10000", 
+        false));
+
+    controller_task.await?;
+    cmd_convert_task.await?;
+    cmd_to_wheel.await?;
+    wheel_to_esp32.await?;
 
     Ok(())
 }
