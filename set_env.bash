195a9fb7ff8b3e23723b0c95d725515359889285
motii <108280115+motii8128@users.noreply.github.com>
2024-03-28 18:21:19 +0900

    Create set_env.bash

---
+++ set_env.bash
@@ -0,0 +1,4 @@
+sudo apt install libudev-dev
+cd /etc/udev/rules.d
+sudo touch ./99-hidraw-permissions.rules
+sudo echo "KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="plugdev"" > ./99-hidraw-permissions.rules
